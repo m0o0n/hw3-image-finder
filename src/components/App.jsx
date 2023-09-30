a53234c25bd4af73268d5e6ed61afd14ba38f1ab
m0o0n <strikunvlad@gmail.com>
2023-09-30 16:33:01 +0300

    fix pagination

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,57 +7,43 @@ import SearchBar from './Searchbar';
 
 export class App extends Component {
   state = {
-    gallery: null,
+    gallery: [],
     filtedGalery: null,
     query: '',
+    totalHits: null,
     page: 1,
     err: '',
-    isLoading: true,
+    isLoading: false,
   };
 
   handleLoadMore = () => {
-    this.setState(prev => ({ page: prev.page + 1 }));
+    this.setState(prev => ({ page: prev.page + 1, isLoading: true }));
   };
 
   submit = value => {
-    fetchImages(this.state.page, value)
-      .then(data => {
-        this.setState({ gallery: data.hits, query: value });
-      })
-      .catch(err => {
-        this.setState({ err, isLoading: false });
-      });
+    this.setState({
+      page: 1,
+      gallery: [],
+      query: value,
+      totalHits: null,
+      isLoading: true
+    })
   };
 
   componentDidUpdate(_, prevState) {
-    if (prevState.page !== this.state.page) {
-      this.setState({ isLoading: true });
-      fetchImages(this.state.page, this.state.query).then(data => {
+    if (prevState.page !== this.state.page || prevState.query !== this.state.query) {
+      fetchImages(this.state.page, this.state.query).then(({hits, totalHits}) => {
         setTimeout(() => {
-          this.setState(prev => ({
-            gallery: [...prev.gallery, ...data.hits],
+          this.setState(({query, gallery}) => ({
+            gallery: query ? [...gallery, ...hits] : [],
             isLoading: false,
-          }));
-        }, 500);
-      });
-    }
-  }
-
-  componentDidMount() {
-    if (!Boolean(this.state.query)) {
-      this.setState({ gallery: [], isLoading: false });
-    }
-    fetchImages(this.state.page)
-      .then(({ hits }) => {
-        setTimeout(() => {
-          this.state.query
-            ? this.setState({ gallery: hits, isLoading: false })
-            : this.setState({ gallery: [], isLoading: false });
-        }, 500);
+            totalHits: totalHits
+          }))
+        }, 500)
+      }).catch(err => {
+        this.setState({err, isLoading: false})
       })
-      .catch(err => {
-        this.setState({ err, isLoading: false });
-      });
+    }
   }
 
   render() {
@@ -71,7 +57,7 @@ export class App extends Component {
             <ImageGallery gallery={this.state.gallery} />
             <Button
               loadMore={this.handleLoadMore}
-              isRender={this.state.gallery.length}
+              isRender={this.state.gallery.length && (this.state.gallery.length < this.state.totalHits)}
               page={this.state.page}
             />
           </>
